974b3cdd5c3197623be6d54e7e3a6a8ca5a3ab9e
onticinc <onticinc@gmail.com>
2023-01-15 16:32:33 -0700

    working on cagetofies page

--- frontend/pages/categories/[slug].js
+++ frontend/pages/categories/[slug].js
@@ -1,10 +1,38 @@
 import React from 'react';
+import Layout from '../../components/Layout';
+import {Box, Card, CardContent, Grid, Link, makeStyles, Typography} from "@material-ui/core";
 
 const Id = () => {
   return (
-    <div>
-    	Slug
-    </div>
+    <Layout>
+    	<Grid container>
+        <Grid item xs={12} md={3}>
+          Todo Filters
+        </Grid>
+        <Grid item xs={12} md={9}>
+          <Card>
+            <Box>
+              <CardContent>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography variant='h5'>Business Name</Typography>
+                    <Typography variant='subtitle1'>$$$</Typography>
+                    <Link variant='subtitle1' href='http://localhost:3000'>Business Website</Link>
+                    <Typography variant="subtitle1">Business Phone</Typography>
+                    <Typography variant="subtitle1">Description</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant='h5'>Todo Reviews</Typography>
+                    <Typography variant='subtitle1'>Business Hours</Typography>
+                    <Typography variant='subtitle1'>Address</Typography>
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Box>
+          </Card>
+        </Grid>
+      </Grid>
+    </Layout>
   )
 }
 
